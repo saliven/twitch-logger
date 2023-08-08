051bd1f7aacc640c78311d3ba236a5309be73a23
Saliven <s4liven@pm.me>
2023-08-08 15:28:11 +0200

    Add caching for slow endpoints

--- src/api/v1/stats.rs
+++ src/api/v1/stats.rs
@@ -1,10 +1,10 @@
 use actix_web::{get, web, Error, HttpResponse};
 
-use crate::{database::stats::Stats, global::GlobalState};
+use crate::{database::stats::get_size, global::GlobalState};
 
 #[get("/stats/size")]
 async fn stats_size(global_data: web::Data<GlobalState>) -> Result<HttpResponse, Error> {
-	let stats = Stats::get_size(&global_data.db).await.unwrap();
+	let stats = get_size(&global_data.db).await.unwrap();
 
 	Ok(HttpResponse::Ok().json(stats))
 }
--- src/database/stats.rs
+++ src/database/stats.rs
@@ -1,24 +1,30 @@
+use cached::proc_macro::cached;
 use serde::Serialize;
 
-#[derive(Debug, sqlx::FromRow, Serialize)]
+#[derive(Debug, sqlx::FromRow, Serialize, Clone)]
 pub struct Stats {
 	total_count: i64,
 	db_size: String,
 }
 
-impl Stats {
-	pub async fn get_size(db: &sqlx::PgPool) -> Result<Self, sqlx::Error> {
-		let total_count = sqlx::query_scalar("SELECT COUNT(*) FROM logs")
-			.fetch_one(db)
-			.await?;
+#[cached(
+	time = 600,
+	time_refresh = true,
+	result = true,
+	key = "bool",
+	convert = r#"{ true }"#
+)]
+pub async fn get_size(db: &sqlx::PgPool) -> Result<Stats, sqlx::Error> {
+	let total_count = sqlx::query_scalar("SELECT COUNT(*) FROM logs")
+		.fetch_one(db)
+		.await?;
 
-		let db_size = sqlx::query_scalar("SELECT pg_size_pretty(pg_database_size(current_database()))")
-			.fetch_one(db)
-			.await?;
+	let db_size = sqlx::query_scalar("SELECT pg_size_pretty(pg_database_size(current_database()))")
+		.fetch_one(db)
+		.await?;
 
-		Ok(Self {
-			total_count,
-			db_size,
-		})
-	}
+	Ok(Stats {
+		total_count,
+		db_size,
+	})
 }
