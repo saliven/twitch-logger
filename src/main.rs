77fed973c9bba61545a53f392c66e954d3cf61e5
Saliven <s4liven@pm.me>
2023-08-08 18:29:00 +0200

    Improve env for local development

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,16 @@ mod utils;
 
 #[tokio::main]
 async fn main() {
+	let env = std::env::var("RUST_ENV").unwrap_or_else(|_| "development".to_string());
+
+	let fallback_log_level = match env.as_str() {
+		"development" => Level::DEBUG,
+		"production" => Level::INFO,
+		_ => Level::DEBUG,
+	};
+
 	let filter_level = std::env::var("RUST_LOG")
-		.map(|s| s.parse().unwrap_or(Level::DEBUG))
+		.map(|s| s.parse().unwrap_or(fallback_log_level))
 		.unwrap_or(Level::DEBUG);
 
 	let subscriber = FmtSubscriber::builder()
@@ -21,17 +29,28 @@ async fn main() {
 
 	tracing::subscriber::set_global_default(subscriber).expect("Failed to set subscriber");
 
-	let url = std::env::var("DATABASE_URL")
-		.unwrap_or_else(|_| "postgres://postgres:postgres@localhost:5432/logging".to_string());
+	info!("Starting up");
 
-	info!("Connecting to database at {}", url);
+	info!("Loading environment variables");
 
-	let db = sqlx::postgres::PgPool::connect(&url).await.unwrap();
+	if env == "development" {
+		dotenv::dotenv().ok();
+	}
+
+	let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+	info!("Connecting to database at {}", database_url);
+
+	let db = sqlx::postgres::PgPool::connect(&database_url)
+		.await
+		.unwrap();
 
 	sqlx::migrate!("./migrations").run(&db).await.unwrap();
 
 	let global = web::Data::new(GlobalState::new(db));
 	let global_http = web::Data::clone(&global);
+
+	info!("Starting main processes");
 
 	tokio::spawn(twitch::chat::start(global));
 	api::start(global_http).await.unwrap();
