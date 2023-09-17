43bee903076b1ab94836f53f91b2a7c5476c78a5
Saliven <s4liven@pm.me>
2023-09-17 02:32:02 +0200

    Add tests

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -20,3 +20,26 @@ pub fn parse_env_list(key: &str) -> Vec<String> {
 	.map(|s| s.to_string())
 	.collect::<Vec<String>>()
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_parse_file() {
+		let data = "test1\ntest2\ntest3";
+		std::fs::write("./test.txt", data).expect("Failed to write file");
+
+		let channels = parse_file("./test.txt");
+		assert_eq!(channels, vec!["test1", "test2", "test3"]);
+
+		std::fs::remove_file("./test.txt").expect("Failed to remove file");
+	}
+
+	#[test]
+	fn test_parse_env_list() {
+		env::set_var("TEST_ENV", "test1,test2,test3");
+		let channels = parse_env_list("TEST_ENV");
+		assert_eq!(channels, vec!["test1", "test2", "test3"]);
+	}
+}
