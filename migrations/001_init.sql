01cf0b454613e61a3e0dd353dec88b4587cd6922
Saliven <s4liven@pm.me>
2023-09-17 02:30:34 +0200

    Update migrations

--- migrations/001_init.sql
+++ migrations/001_init.sql
@@ -1,7 +1,7 @@
--- Tables
+BEGIN;
 
 CREATE TABLE logs (
-    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    id uuid DEFAULT gen_random_uuid(),
     username varchar(32) NOT NULL,
     channel varchar(32) NOT NULL,
     content text,
@@ -9,6 +9,10 @@ CREATE TABLE logs (
     created_at timestamp WITH TIME ZONE DEFAULT NOW()
 );
 
--- Indexes
-
 CREATE INDEX logs_created_at_idx ON logs (created_at);
+CREATE INDEX logs_username_idx ON logs (username);
+CREATE INDEX logs_channel_idx ON logs (channel);
+
+SELECT create_hypertable('logs', 'created_at');
+
+COMMIT;
